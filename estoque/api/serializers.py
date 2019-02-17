473da554db09d8ff4584458c8ac9d494be422699
= <=>
2019-02-17 19:30:39 -0300

    Adding SlugRelatedField to represent the API fields

--- estoque/api/serializers.py
+++ estoque/api/serializers.py
@@ -2,6 +2,24 @@ from rest_framework import serializers
 from estoque.models import Componente
 
 class ComponenteSerializer(serializers.ModelSerializer):
+    tipo = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='nome'
+    )
+
+    local = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='nome'
+    )
+
+    fabricante = serializers.SlugRelatedField(
+        many=False,
+        read_only=True,
+        slug_field='nome'
+    )
+
     class Meta:
         model = Componente
         fields = [
